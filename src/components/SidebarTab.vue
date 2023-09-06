8f72307fa5b373f23a3bc9c0c076a0be94a14762
jirubizu <jirubizu@proton.me>
2023-09-06 18:10:07 +0100

    wip

---
+++ src/components/SidebarTab.vue
@@ -0,0 +1,29 @@
+<template>
+    <li @click="selectTab(props.tabName)" class="rounded-sm main-color-hover">
+        <a href="#" class="flex items-center p-2 space-x-3 rounded-md">
+            <!-- <slot name="icon"></slot> -->
+            <v-icon :name="props.displayIcon"/>
+            <span>{{ props.tabName.toLocaleUpperCase() }}</span>
+        </a>
+    </li>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+interface Props {
+    tabName: string;
+    displayIcon: string;
+}
+
+const emits = defineEmits(['selected']);
+const selectedTab = ref('models');
+const props = defineProps<Props>();
+
+const icon = props.displayIcon;
+let displayName = props.tabName;
+
+const selectTab = (tabName: string) => {
+    selectedTab.value = tabName;
+    emits('selected', tabName);
+}
+</script>
